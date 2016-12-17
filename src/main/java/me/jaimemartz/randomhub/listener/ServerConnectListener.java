0d9f6935fe9d7a617d794a1c48773d9f19490634
Jaime Martinez Rincon <mrjaime1999@gmail.com>
2016-12-17 13:56:30 +0100

    Added support for lobby switch plugins

--- src/main/java/me/jaimemartz/randomhub/listener/ServerConnectListener.java
+++ src/main/java/me/jaimemartz/randomhub/listener/ServerConnectListener.java
@@ -4,6 +4,7 @@ import me.jaimemartz.randomhub.ConnectionAttempt;
 import me.jaimemartz.randomhub.RandomHub;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -19,14 +20,17 @@ public class ServerConnectListener implements Listener {
     public void on(ServerConnectEvent event) {
         ProxiedPlayer player = event.getPlayer();
         ServerInfo target = event.getTarget();
+        Server server = player.getServer();
 
-        if (plugin.getServers().contains(target)) {
-            new ConnectionAttempt(plugin, player) {
-                @Override
-                public void connect(ServerInfo server) {
-                    event.setTarget(server);
-                }
-            };
+        if (server != null && !plugin.getServers().contains(server.getInfo())) {
+            if (plugin.getServers().contains(target)) {
+                new ConnectionAttempt(plugin, player) {
+                    @Override
+                    public void connect(ServerInfo server) {
+                        event.setTarget(server);
+                    }
+                };
+            }
         }
     }
 }
